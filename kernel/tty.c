0ad9877b9305c7b0e1b9730a735cfe8274c76910
梁斌 <waterstonelb@gmail.com>
2019-11-27 00:22:17 +0800

    debug

--- kernel/tty.c
+++ kernel/tty.c
@@ -26,7 +26,7 @@ PRIVATE int esc_mode;
 PUBLIC char keys[80 * 25] = {0};
 PUBLIC char s_keys[80 * 25] = {0};
 PUBLIC int pos[80 * 25] = {0};
-PUBLIC char delete[80*25]={0};
+PUBLIC char delete[80 * 25] = {0};
 PUBLIC int d_index;
 PUBLIC int index;
 PUBLIC int p_index;
@@ -37,25 +37,27 @@ PUBLIC int s_index;
  *======================================================================*/
 PUBLIC void task_tty()
 {
-        clean_screen();
-        esc_mode = 0;
-        index = 0;
-        d_index=0;
-        s_index = 0;
-        p_index = 0;
-        time = get_ticks();
-        while (1)
-        {
-                if (((get_ticks() - time) * 1000 / HZ) > 200000 && esc_mode == 0)
-                {
-                        clean_screen();
-                        index = 0;
-                        s_index = 0;
-                        time = get_ticks();
-                }
-                keyboard_read();
-                set_cursor(disp_pos);
-        }
+	clean_screen();
+	esc_mode = 0;
+	index = 0;
+	d_index = 0;
+	s_index = 0;
+	p_index = 0;
+	time = get_ticks();
+	while (1)
+	{
+		// if (((get_ticks() - time) * 1000 / HZ) > 200000 && esc_mode == 0)
+		// {
+		// 	clean_screen();
+		// 	index = 0;
+		// 	d_index = 0;
+		// 	s_index = 0;
+		// 	p_index = 0;
+		// 	time = get_ticks();
+		// }
+		keyboard_read();
+		set_cursor(disp_pos);
+	}
 }
 
 /*======================================================================*
@@ -63,177 +65,184 @@ PUBLIC void task_tty()
  *======================================================================*/
 PUBLIC void in_process(u32 key)
 {
-        char output[2] = {'\0', '\0'};
-        if (esc_mode)
-        {
-                if (!(key & FLAG_EXT))
-                {
-                        if (!(key & FLAG_CTRL_L || key & FLAG_CTRL_R))
-                        {
-                                output[0] = key & 0xFF;
-                                s_keys[s_index++] = output[0];
-                                disp_color_str(output, BLUE);
-                        }
-                }
-                else
-                {
-                        int raw_code = key & MASK_RAW;
-                        switch (raw_code)
-                        {
-                        case ENTER:
-                                s_keys[s_index] = '\0';
-                                clean_screen();
-                                str_match();
-                                disp_color_str(s_keys, BLUE);
-                                break;
-                        case TAB:
-                                s_keys[s_index++] = '\t';
-                                int num = 4 - (disp_pos / 2 % 4);
-                                for (int i = 0; i < num; i++)
-                                        disp_str(" ");
-                                break;
-                        case BACKSPACE:
-                                if (s_index > 0)
-                                {
-                                        disp_pos -= 2;
-                                        disp_str(" ");
-                                        disp_pos -= 2;
-                                        s_keys[--s_index] = '\0';
-                                }
-                                break;
-                        case ESC:
-                                esc_mode = 0;
-                                s_index = 0;
-                                refresh_screen();
-                                break;
-                        default:
-                                break;
-                        }
-                }
-        }
-        else
-        {
-                if (!(key & FLAG_EXT))
-                {
-                        if (key & FLAG_CTRL_L || key & FLAG_CTRL_R)
-                        {
-                                back();
-                        }
-                        else
-                        {
-                                output[0] = key & 0xFF;
-                                keys[index] = output[0];
-                                pos[p_index++] = ++index;
-                                disp_str(output);
-                        }
-                }
-                else
-                {
-                        int raw_code = key & MASK_RAW;
-                        switch (raw_code)
-                        {
-                        case ENTER:
-                                keys[index] = '\n';
-                                pos[p_index++] = ++index;
-                                disp_str("\n");
-                                break;
-                        case TAB:
-                                keys[index] = '\t';
-                                pos[p_index++] = ++index;
-                                int num = 4 - (disp_pos / 2 % 4);
-                                for (int i = 0; i < num; i++)
-                                {
-                                        disp_str(" ");
-                                }
-                                break;
-                        case BACKSPACE:
-                                if (index > 0)
-                                {
-                                        pos[p_index++] = --index;
-                                        delete[d_index++]=keys[index];
-                                        refresh_screen();
-                                }
-                                break;
-                        case ESC:
-                                esc_mode = 1;
-                                break;
-                        default:
-                                break;
-                        }
-                }
-        }
+	char output[2] = {'\0', '\0'};
+	if (esc_mode)
+	{
+		if (!(key & FLAG_EXT))
+		{
+			if (!(key & FLAG_CTRL_L || key & FLAG_CTRL_R))
+			{
+				output[0] = key & 0xFF;
+				s_keys[s_index++] = output[0];
+				disp_color_str(output, BLUE);
+			}
+		}
+		else
+		{
+			int raw_code = key & MASK_RAW;
+			switch (raw_code)
+			{
+			case ENTER:
+				s_keys[s_index] = '\0';
+				clean_screen();
+				str_match();
+				disp_color_str(s_keys, BLUE);
+				break;
+			case TAB:
+				s_keys[s_index++] = '\t';
+				int num = 4 - (disp_pos / 2 % 4);
+				for (int i = 0; i < num; i++)
+					disp_str(" ");
+				break;
+			case BACKSPACE:
+				if (s_index > 0)
+				{
+					disp_pos -= 2;
+					disp_str(" ");
+					disp_pos -= 2;
+					s_keys[--s_index] = '\0';
+				}
+				break;
+			case ESC:
+				esc_mode = 0;
+				s_index = 0;
+				refresh_screen();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	else
+	{
+		if (!(key & FLAG_EXT))
+		{
+			if ((key & FLAG_CTRL_L || key & FLAG_CTRL_R) && (key & MASK_RAW) == 'z')
+			{
+				back();
+			}
+			else
+			{
+				output[0] = key & 0xFF;
+				keys[index] = output[0];
+				pos[p_index++] = ++index;
+				disp_str(output);
+			}
+		}
+		else
+		{
+			int raw_code = key & MASK_RAW;
+			switch (raw_code)
+			{
+			case ENTER:
+				keys[index] = '\n';
+				pos[p_index++] = ++index;
+				disp_pos = 160 * (disp_pos / 160 +1);
+				set_cursor(disp_pos);
+				break;
+			case TAB:
+				keys[index] = '\t';
+				pos[p_index++] = ++index;
+				int num = 4 - (disp_pos / 2 % 4);
+				for (int i = 0; i < num; i++)
+				{
+					disp_str(" ");
+				}
+				break;
+			case BACKSPACE:
+				if (index > 0)
+				{
+					pos[p_index++] = --index;
+					delete[d_index++] = keys[index];
+					refresh_screen();
+				}
+				break;
+			case ESC:
+				esc_mode = 1;
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
 PRIVATE void back()
 {
-        p_index -= 1;
-        index = pos[p_index - 1];
-        if(pos[p_index]<pos[p_index-1])
-                keys[index-1]=delete[--d_index];
-        refresh_screen();
+	if (p_index == 0 && d_index == 0)
+		return;
+	p_index--;
+	index = p_index > 0 ? pos[p_index - 1] : 0;
+	if (p_index > 0 && pos[p_index] < pos[p_index - 1])
+	{
+
+		keys[index - 1] = delete[--d_index];
+	}
+	refresh_screen();
 }
 PRIVATE void refresh_screen()
 {
-        clean_screen();
-        char output[2] = {'\0', '\0'};
-        for (int i = 0; i < index; i++)
-        {
-                if (keys[i] == '\n')
-                {
-                        disp_str("\n");
-                }
-                else if (keys[i] == '\t')
-                {
-                        int num = 4 - (disp_pos / 2 % 4);
-                        for (int i = 0; i < num; i++)
-                                disp_str(" ");
-                }
-                else
-                {
-                        output[0] = keys[i];
-                        disp_str(output);
-                }
-        }
+	clean_screen();
+	char output[2] = {'\0', '\0'};
+	for (int i = 0; i < index; i++)
+	{
+		if (keys[i] == '\n')
+		{
+			disp_pos = 160 * (disp_pos / 160 +1);
+			set_cursor(disp_pos);
+		}
+		else if (keys[i] == '\t')
+		{
+			int num = 4 - (disp_pos / 2 % 4);
+			for (int i = 0; i < num; i++)
+				disp_str(" ");
+		}
+		else
+		{
+			output[0] = keys[i];
+			disp_str(output);
+		}
+	}
 }
 PRIVATE void str_match()
 {
-        int is_match = 0;
-        char output[2] = {'\0', '\0'};
-        for (int i = 0; i < index; i++)
-        {
-                int j = i;
-                while (s_keys[j - i] == keys[j++])
-                        ;
-                if (j - i - s_index > 0)
-                {
-                        for (int k = i; k < j - 1; k++)
-                        {
-                                output[0] = keys[k];
-                                disp_color_str(output, BLUE);
-                                i = k;
-                        }
-                }
-                else
-                {
-                        output[0] = keys[i];
-                        disp_str(output);
-                }
-        }
+	int is_match = 0;
+	char output[2] = {'\0', '\0'};
+	for (int i = 0; i < index; i++)
+	{
+		int j = i;
+		while (s_keys[j - i] == keys[j++])
+			;
+		if (j - i - s_index > 0)
+		{
+			for (int k = i; k < j - 1; k++)
+			{
+				output[0] = keys[k];
+				disp_color_str(output, BLUE);
+				i = k;
+			}
+		}
+		else
+		{
+			output[0] = keys[i];
+			disp_str(output);
+		}
+	}
 }
 PRIVATE void clean_screen()
 {
-        disp_pos = 0;
-        for (int i = 0; i < 80 * 25; i++)
-        {
-                disp_str(" ");
-        }
-        disp_pos = 0;
+	disp_pos = 0;
+	for (int i = 0; i < 80 * 25; i++)
+	{
+		disp_str(" ");
+	}
+	disp_pos = 0;
 }
 PRIVATE void set_cursor(unsigned int position)
 {
-        disable_int();
-        out_byte(CRTC_ADDR_REG, CURSOR_H);
-        out_byte(CRTC_DATA_REG, (position / 2 >> 8) & 0xFF);
-        out_byte(CRTC_ADDR_REG, CURSOR_L);
-        out_byte(CRTC_DATA_REG, position / 2 & 0xFF);
-        enable_int();
+	disable_int();
+	out_byte(CRTC_ADDR_REG, CURSOR_H);
+	out_byte(CRTC_DATA_REG, (position / 2 >> 8) & 0xFF);
+	out_byte(CRTC_ADDR_REG, CURSOR_L);
+	out_byte(CRTC_DATA_REG, position / 2 & 0xFF);
+	enable_int();
 }
